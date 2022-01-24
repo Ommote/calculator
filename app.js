0d0df280c320eed0e24dd3a4a40111ff100e0c75
Ommote <olivia.motevalli@gmail.com>
2022-01-24 21:43:55 +0000

    created pseudo code

---
+++ app.js
@@ -0,0 +1,9 @@
+// Calculator App
+
+// 1. must take number inputs (as a minimum)
+
+// 2. must take an operator
+
+// 3. must perform a calculation based on number and operator INPUTS
+
+// 4. must OUTPUT a correct result
