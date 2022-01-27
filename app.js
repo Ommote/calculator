2aba36d6021e83058e14d0b7f5f414b04e73b8a1
Ommote <olivia.motevalli@gmail.com>
2022-01-27 16:52:48 +0000

    re added decimal point and fixed display

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-// Calculator App
-
-// 1. must take number inputs (as a minimum)
-
-// 2. must take an operator
-
-// 3. must perform a calculation based on number and operator INPUTS
-
-// 4. must OUTPUT a correct result
 
 // VARIABLES
 // display
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -1,10 +1,5 @@
 "use strict";
 
-// Calculator App
-// 1. must take number inputs (as a minimum)
-// 2. must take an operator
-// 3. must perform a calculation based on number and operator INPUTS
-// 4. must OUTPUT a correct result
 // VARIABLES
 // display
 var display = document.getElementById("display"); // number buttons
